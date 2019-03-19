c5f8885f5b94da1e3832b029a4c27972f9dfc789
Riccardo Belliato <belliatoriccardo@gmail.com>
2019-03-19 16:17:04 +0100

    IntSList

--- src/app/EsempiJava_2019_3_19.java
+++ src/app/EsempiJava_2019_3_19.java
@@ -2,8 +2,12 @@ package app;
 
 /**
  * EsempiJava_2019_3_19
+ * Vedi IntSList
  */
 public class EsempiJava_2019_3_19 {
 
-  
+  public static void main(String[] args) {
+  }
+
+
 }
---
+++ src/app/IntSList.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * IntSList s = new IntSList();   (list)
+ * 
+ * s.nullList()     -> boolean    (null? s)
+ * s.car()          -> int        (car s)
+ * s.cdr()          -> IntSList   (cdr s)
+ * s.cons(int n)    -> IntSList   (cons s n)
+ */
+public class IntSList {
+  
+} // class IntSList
