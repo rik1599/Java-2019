63e0f992670a089073719062a78705d66c13e7f6
Riccardo Belliato <belliatoriccardo@gmail.com>
2019-03-19 19:29:55 +0100

    IntSList completato

--- src/app/EsempiJava_2019_3_19.java
+++ src/app/EsempiJava_2019_3_19.java
@@ -7,6 +7,15 @@ package app;
 public class EsempiJava_2019_3_19 {
 
   public static void main(String[] args) {
+    IntSList s = range(1, 100);
+    while (!s.nullList()) {
+      System.out.println(s.car());
+      s = s.cdr();
+    }
+  }
+
+  public static IntSList range(int inf, int sup) {
+    return inf > sup ? new IntSList() : range(inf + 1, sup).cons(inf);
   }
 
 
--- src/app/IntSList.java
+++ src/app/IntSList.java
@@ -9,7 +9,37 @@ package app;
  * s.cons(int n)    -> IntSList   (cons s n)
  */
 public class IntSList {
+
+  //#region variabili
+  private boolean empty;
+  private int first;
+  private IntSList rest;
+
   public IntSList() {
-    
+    this.empty = true;
+    this.first = 0;
+    this.rest = null;
+  }
+
+  public IntSList(int n, IntSList s) {
+    this.empty = false;
+    this.first = n;
+    this.rest = s;
+  }
+
+  public boolean nullList() {
+    return this.empty;
+  }
+
+  public int car() {
+    return this.first;
+  }
+
+  public IntSList cdr() {
+    return rest;
+  }
+
+  public IntSList cons(int n) {
+    return new IntSList(n, this);
   }
 } // class IntSList
