ba1eb9cfa1546fb3cf5e518f45caab14beacfce6
Riccardo Belliato <belliatoriccardo@gmail.com>
2019-05-07 23:08:09 +0200

    lezione del 7 maggio

---
+++ src/huffman_project/EsempioIO.java
@@ -0,0 +1,40 @@
+package huffman_project;
+
+import huffman_toolkit.*;
+
+public class EsempioIO {
+
+    public static void main(String[] args) {
+        copia("src/huffman_project/file.txt", "src/huffman_project/Copia.txt");
+    }
+
+    public static void copia(String tf1, String tf2) {
+        InputTextFile itf = new InputTextFile(tf1);
+        OutputTextFile otf = new OutputTextFile(tf2);
+
+        while (itf.textAvailable()) {
+            String txt = itf.readTextLine();
+            otf.writeTextLine(txt);
+        }
+
+        itf.close();
+        otf.close();
+    }
+
+    public static int copiaLinee(String tf1, String tf2) {
+        InputTextFile itf = new InputTextFile(tf1);
+        OutputTextFile otf = new OutputTextFile(tf2);
+
+        int count = 0;
+
+        while (itf.textAvailable()) {
+            String txt = itf.readTextLine();
+            otf.writeTextLine(txt);
+            count++;
+        }
+
+        itf.close();
+        otf.close();
+        return count;
+    }
+}
